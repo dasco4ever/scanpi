503a34aa5389bbe124829d7bba2d32dc21b761e2
Dasco4Ever <141959753+dasco4ever@users.noreply.github.com>
2024-12-28 10:29:46 +0100

    Update scan_adf.sh
    Updated to my needs

--- scan_adf.sh
+++ scan_adf.sh
@@ -2,7 +2,7 @@
 set -x
 # set -e
 
-SCAN_DIRECTORY=${SCAN_DIRECTORY:-/mnt/scan}
+SCAN_DIRECTORY=${SCAN_DIRECTORY:-/home/dasco/scanpi}
 PROCESSING_LOCKFILE=/var/lock/.scanlock
 touch "$PROCESSING_LOCKFILE"
 
@@ -14,28 +14,31 @@ flock 4
 pushd $SCAN_DIRECTORY
 mkdir -p "$FILENAME"
 pushd "$FILENAME"
-
+#
+# cd "$FILENAME"
 # Scan the document
-scanadf --mode $MODE --source "$SOURCE" --resolution $RESOLUTION 2>>stderr.log 1>>stdout.log || true
+#scanadf --mode $MODE --source "$SOURCE" --resolution $RESOLUTION # 2>>stderr.log 1>>stdout.log || true
+scanimage --output-file image-0001 --resolution $RESOLUTION --mode $MODE --format jpeg # 2>>stderr.log 1>>stdout.log ||>#scanimage --resolution $RESOLUTION --mode $MODE --format jpeg # 2>>stderr.log 1>>stdout.log || true
 
 # Convert to pdf and apply OCR
-convert image* "$FILENAME.pdf" 2>>stderr.log 1>>stdout.log
-ocrmypdf -r -d -c --rotate-pages-threshold 0 "$FILENAME.pdf" "$FILENAME.ocr.pdf" 2>>stderr.log 1>>stdout.log
+convert image* "$FILENAME.pdf" # 2>>stderr.log 1>>stdout.log
+ocrmypdf -r -d -c --rotate-pages-threshold 0 "$FILENAME.pdf" "$FILENAME.ocr.pdf" # 2>>stderr.log 1>>stdout.log
 # Sometimes ocrmypdf still has these files locked after, wait until they're done
 until ! lsof "stdout.log" >/dev/null 2>&1; do sleep 1s; done
 until ! lsof "stderr.log" >/dev/null 2>&1; do sleep 1s; done
 
 # Replace non-ocr with ocr document and cleanup files and locks
-mv "$FILENAME.ocr.pdf" "$FILENAME.pdf" 2>>stderr.log 1>>stdout.log
-rm image* 2>>stderr.log 1>>stdout.log
+mv "$FILENAME.ocr.pdf" "$FILENAME.pdf" # 2>>stderr.log 1>>stdout.log
+rm image* # 2>>stderr.log 1>>stdout.log
 
 # Ensure permissions allow users to access the scans
 chmod -R u+rwX,g+rwX,o+rwX  "$FILENAME"
 
 # Give a break between scan jobs of 15 seconds to allow loading a queued job
-sleep 15s
+#sleep 15s
 
-exec 4<&- 
+# exec 4<&-
 popd # FILENAME
 
 popd # SCAN_DIRECTORY
+#cd ..
